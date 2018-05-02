a2c221049d907c690d175e073dfe798cd42684ce
Diego777LM10 <xavimaster777@gmail.com>
2018-05-02 01:52:38 -0200

    Add files via upload

--- haar.cpp
+++ haar.cpp
@@ -12,26 +12,29 @@ class imagenes{
 		float minimo;
 		float maximo;
 		float promedio;
+		float varianza;
 		CImg<float> imagen;
 		vector<float> caract;//vector que contiene las caracteristicas
 		string nombre;//nombre del archivo
 		vector<string> name_imagenes;
 		map<string , vector<float> > imag_matriz;
-
+		int tipo;
 	public:
-		imagenes();
+		imagenes(int t);
 		void insertar(string name);
 		void quad(int num);
 		void caracteristica();
 		float get_minimo();
 		float get_maximo();
 		float get_promedio();
+		float get_varianza();
 		float get_desviacion();
 		void generar(string archivo,int num);
 		void all_caracteristicas();
 		void mostrar();
 };
-imagenes ::imagenes(){
+imagenes ::imagenes(int t){
+	tipo=t;
 	desviacion=minimo=maximo=promedio=0;
 	nombre ="";
 }
@@ -42,7 +45,7 @@ void imagenes:: insertar(string name){
 void imagenes::quad(int num){
 	imagen =(imagen.haar(false,num)).crop(0,0, imagen.width()/pow(2,num), imagen.height()/pow(2,num));
 	//imagen.haar(false,num);
-	mostrar();
+	//mostrar();
 }
 void imagenes::caracteristica(){
 	minimo =get_minimo();
@@ -53,14 +56,15 @@ void imagenes::caracteristica(){
 	caract.push_back(maximo);
 	caract.push_back(promedio);
 	caract.push_back(desviacion);
-
+	 
+       
 	for(int i=0;i<caract.size();i++)
 		cout<<caract[i]<<"  ";
 	imag_matriz.insert(make_pair(nombre,caract));
 	caract.clear();
 }
 void imagenes::all_caracteristicas(){
-
+	ofstream myfile;
 	float mini = imagen(0,0);
    	float maxi = imagen(0,0);
 	float prom = 0.0;
@@ -80,15 +84,23 @@ void imagenes::all_caracteristicas(){
    		for(int j=0;j<=imagen.height()-1;j++)
             count +=pow(imagen(i,j)-promedio,2);
     desviacion =sqrt(count/256);
-
+    varianza=(count/256);
     caract.push_back(minimo);
     caract.push_back(maximo);
-    caract.push_back(promedio);
+    caract.push_back(varianza);
     caract.push_back(desviacion);
-
+	 myfile.open ("caract.txt",ios::app);
 	for(int i=0;i<caract.size();i++)
-		cout<<caract[i]<<"  ";
-	cout<<endl;
+		myfile << caract[i]<<",";
+		//cout<<caract[i]<<"  ";
+	//cout<<endl;
+	if(tipo==1)
+		myfile <<"1";
+	else if(tipo==0)
+		myfile <<"0";
+	myfile<<endl;
+	myfile.close();
+
 	imag_matriz.insert(make_pair(nombre,caract));
 	caract.clear();
 
@@ -127,6 +139,14 @@ float imagenes:: get_desviacion(){
             count +=pow(imagen(i,j)-prom,2);
     return sqrt(count/256);
 }
+float imagenes:: get_varianza(){
+	float prom =get_promedio();
+	float count=0.0;
+	for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+            count +=pow(imagen(i,j)-prom,2);
+    return (count/256);
+}
 void imagenes:: generar(string archivo,int num){
 	ifstream file(archivo.c_str());
 	char buffer[1000];
@@ -150,7 +170,9 @@ void imagenes:: mostrar(){
 	(imagen).display();
 }
 int main(){
-	imagenes img;
-	img.generar("gatos.txt",3);
+	imagenes gatos(1);
+	gatos.generar("gatos.txt",3);
+	imagenes perros(0);
+	perros.generar("perros.txt",3);
 	//g++ haar.cpp -w -lpthread -lX11 -o main
 }
