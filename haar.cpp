fda2f5386e178c0d5d2811be7a452d414648553e
Diego777LM10 <xavimaster777@gmail.com>
2018-05-02 00:33:03 -0200

    Add files via upload

---
+++ haar.cpp
@@ -0,0 +1,156 @@
+#include <CImg.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
+using namespace std;
+using namespace cimg_library;
+
+class imagenes{
+	public: 
+		float desviacion;
+		float minimo;
+		float maximo;
+		float promedio;
+		CImg<float> imagen;
+		vector<float> caract;//vector que contiene las caracteristicas
+		string nombre;//nombre del archivo
+		vector<string> name_imagenes;
+		map<string , vector<float> > imag_matriz;
+
+	public:
+		imagenes();
+		void insertar(string name);
+		void quad(int num);
+		void caracteristica();
+		float get_minimo();
+		float get_maximo();
+		float get_promedio();
+		float get_desviacion();
+		void generar(string archivo,int num);
+		void all_caracteristicas();
+		void mostrar();
+};
+imagenes ::imagenes(){
+	desviacion=minimo=maximo=promedio=0;
+	nombre ="";
+}
+void imagenes:: insertar(string name){
+	nombre=name;
+	imagen.load(name.c_str());
+}
+void imagenes::quad(int num){
+	imagen =(imagen.haar(false,num)).crop(0,0, imagen.width()/pow(2,num), imagen.height()/pow(2,num));
+	//imagen.haar(false,num);
+	mostrar();
+}
+void imagenes::caracteristica(){
+	minimo =get_minimo();
+	maximo =get_maximo();
+	promedio =get_maximo();
+	desviacion =get_desviacion();
+	caract.push_back(minimo);
+	caract.push_back(maximo);
+	caract.push_back(promedio);
+	caract.push_back(desviacion);
+
+	for(int i=0;i<caract.size();i++)
+		cout<<caract[i]<<"  ";
+	imag_matriz.insert(make_pair(nombre,caract));
+	caract.clear();
+}
+void imagenes::all_caracteristicas(){
+
+	float mini = imagen(0,0);
+   	float maxi = imagen(0,0);
+	float prom = 0.0;
+	for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++){
+   			prom += imagen(i,j);
+        	if(imagen(i,j)<mini)
+            	mini = imagen(i,j);
+            if(imagen(i,j)>maxi)
+            	maxi = imagen(i,j);
+   		}
+   	minimo =mini;
+   	maximo =maxi;
+   	promedio= prom*1.0/256;
+   	float count=0.0;
+	for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+            count +=pow(imagen(i,j)-promedio,2);
+    desviacion =sqrt(count/256);
+
+    caract.push_back(minimo);
+    caract.push_back(maximo);
+    caract.push_back(promedio);
+    caract.push_back(desviacion);
+
+	for(int i=0;i<caract.size();i++)
+		cout<<caract[i]<<"  ";
+	cout<<endl;
+	imag_matriz.insert(make_pair(nombre,caract));
+	caract.clear();
+
+}
+float imagenes:: get_minimo(){
+	 float mini = imagen(0,0);
+   for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+        	if(imagen(i,j)<mini)
+            	mini = imagen(i,j);
+    return mini;
+
+}
+float imagenes:: get_maximo(){
+   float maxi = imagen(0,0);
+   for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+        	if(imagen(i,j)>maxi)
+            	maxi = imagen(i,j);
+    return maxi;
+
+}
+float imagenes:: get_promedio(){
+	float prom = 0.0;
+   for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+            prom += imagen(i,j);
+    return prom/256;
+
+}
+float imagenes:: get_desviacion(){
+	float prom =get_promedio();
+	float count=0.0;
+	for(int i=0; i<=imagen.width()-1;i++)
+   		for(int j=0;j<=imagen.height()-1;j++)
+            count +=pow(imagen(i,j)-prom,2);
+    return sqrt(count/256);
+}
+void imagenes:: generar(string archivo,int num){
+	ifstream file(archivo.c_str());
+	char buffer[1000];
+	while(!file.eof()){
+		file.getline(buffer,1000);
+		name_imagenes.push_back(buffer);
+		//cout<<buffer<<endl;
+	}
+	file.close();
+
+	for (int i = 0; i < name_imagenes.size()-1; ++i)
+	{
+		//cout<<i<<endl;
+		insertar(name_imagenes[i]);
+		quad(num);
+		all_caracteristicas();
+	}
+	
+}
+void imagenes:: mostrar(){
+	(imagen).display();
+}
+int main(){
+	imagenes img;
+	img.generar("gatos.txt",3);
+	//g++ haar.cpp -w -lpthread -lX11 -o main
+}
